9fec99903870a34e266759eb3e140c9ea70a60e2
Ubuntu <ubuntu@ip-172-31-22-221.us-west-2.compute.internal>
2024-06-20 20:47:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Web3 from 'web3'
+import HelloAbi from './contractsData/Hello.json'
+import HelloAddress from './contractsData/Hello-address.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    componentWillMount(){
+        this.loadBlockchainData()
+    }
+
+async loadBlockchainData(){
+    console.log('HelloAddress.address', HelloAddress.address)
+    console.log('HelloAbi.abi', HelloAbi.abi)
+    let NewHelloAbi = require('./contractsData/Hello.json');
+    const web3 = new Web3(new Web3.providers.HttpProvider("http://ec2-52-37-183-194.us-west-2.compute.amazonaws.com:8545"))
+    var account;
+    const accounts  = await web3.eth.getAccounts()
+    console.log(accounts)
+    web3.eth.getAccounts().then((f) => {
+        account = f[0];
+    })
+
+    this.setState( { account : accounts[0] })
+    console.log(account);
+    const contract = new web3.eth.Contract(NewHelloAbi.abi);
+    contract.options.address = HelloAddress.address
+    this.setState( { contract })
+
+}
+
+    constructor(props){
+        super(props)
+        console.log("constructor")
+        this.state = {
+            account: '',
+            loading: true,
+            message: ''
+        }
+     }
+
+    setHandler = (event) => {
+          event.preventDefault();
+          console.log('sending ' + event.target.setText.value + ' to the contract');
+          this.state.contract.methods.set(event.target.setText.value).send({ from: this.state.account });
+     }
+
+    getCurrentVal = async () => {
+          let val = await this.state.contract.methods.get().call(console.log);
+          console.log("val", val)
+          this.setState( { message : val })
+    }
+
+
+
+render(){
+
+        return (
+                <div>
+                <h5>message output: {this.state.message}</h5>
+                <h4> {"Get/Set Contract interaction"} </h4>
+                        <form onSubmit={this.setHandler}>
+                                <input id="setText" type="text"/>
+                                <button type={"submit"}> Update Contract </button>
+                        </form>
+                        <div>
+                        <button onClick={this.getCurrentVal} style={{marginTop: '5em'}}> Get Current Contract Value </button>
+                        </div>
+                </div>
+        );
+}
 }
 
 export default App;
